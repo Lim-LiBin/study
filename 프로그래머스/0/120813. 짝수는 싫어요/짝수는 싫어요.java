d9071f812ce530e5258955a27416fe9f45ee6dc8
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-11-29 15:44:27 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] solution(int n) {
         int[] answer = new int[(n+1)/2];
         
-        int index = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 != 0) {
-                answer[index] = i;
-                index++;
-            }
+        int num = 1;
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = num;
+            num = num + 2;
         }
         
         return answer;
