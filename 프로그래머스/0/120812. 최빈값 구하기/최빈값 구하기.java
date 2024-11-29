b54f1ba59d485bbcb9e861cd0c783c8a013be62a
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-11-29 15:29:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.21 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = -1;
+        
+        HashMap<Integer, Integer> frequencyMap = new HashMap();
+        
+        for(int num : array) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        
+        int maxFrequency = 0;
+        boolean isDuplicate = false;
+        
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            
+            if (value > maxFrequency) {
+                maxFrequency = value;
+                answer = key;
+                isDuplicate = false;
+            } else if (value == maxFrequency) {
+                isDuplicate = true;
+            }
+        }
+        
+        if (isDuplicate) return -1;
+        
+        return answer;
+    }
+}
