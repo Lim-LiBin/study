f35a4cc8be412cf7b270ba5238f1d3a9df88ee07
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-02 22:03:35 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int money) {
+        int cup = money / 5500;
+        int mon = money - (5500 * cup);
+        
+        int[] answer = {cup, mon};
+        
+        return answer;
+    }
+}
