06da76879b7f677a87e04257c6870356f4006d3a
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-04 21:14:16 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
+        int answer = n;
         
-        for (int i = 0; i < t; i++) {
-            answer = n * 2;
-            n = answer;
+        for (int i = 1; i <= t; i++) {
+            answer *= 2;
         }
         
         return answer;
