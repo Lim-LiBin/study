63f92c24d5ee9ee3cc07549a4f3d1710b728b374
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-06 17:47:28 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        int length = Math.max(s1.length, s2.length);
         
-        if(length == s1.length) {
-            for(int i = 0; i < s2.length; i++) {
-                for(int j = 0; j < s1.length; j++) {
-                    if(s2[i].equals(s1[j])) answer++;
-                }
-            }
-        } else {
-            for(int i = 0; i < s1.length; i++) {
-                for(int j = 0; j < s2.length; j++) {
-                    if(s1[i].equals(s2[j])) answer++;
-                }
+        for(String str1 : s1) {
+            for(String str2 : s2) {
+                if(str1.equals(str2))
+                    answer++;
             }
         }
         
