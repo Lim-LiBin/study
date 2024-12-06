ce11c6eeb60dc28c0bfaa114fd5ccd48e2559dc1
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-06 16:46:35 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 0;
-        int width = 0;
-        int heigth = 0;
-        int depth = 0;
-        
-        width = box[0] / n;
-        heigth = box[1] / n;
-        depth = box[2] / n;
-        
-        answer = width * heigth * depth;
+        int answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
         
         return answer;
     }
