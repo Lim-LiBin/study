bb6c5c9c23906d40286a27d14b479705c9c8c1b6
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-06 17:14:39 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String str = Integer.toString(n);
+        char []array = str.toCharArray();
+        
+        for(int i = 0; i < array.length; i++) {
+            answer += array[i] - '0';
+        }
+        
+        return answer;
+    }
+}
