b7237801cc6cbd2c6fd2225ea864268ff8f58860
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-23 21:38:24 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.06 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -4,7 +4,7 @@ class Solution {
         int[] cnt = new int[200];
         
         for(int[] i : lines) {
-            int start = i[0]+100;
+            int start = i[0]+100; //[3, 5]일 경우 103번째 인덱스에 저장
             int end = i[1]+100;
             
             for(int k = start; k < end; k++) {
