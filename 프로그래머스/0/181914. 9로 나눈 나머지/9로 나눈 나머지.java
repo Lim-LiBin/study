f3f3ae44289b1703aee6a39cc0de8ae09f79ad68
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-29 21:50:16 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 8.06 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -4,7 +4,7 @@ class Solution {
         int sum = 0;
         
         for(int i = 0; i < number.length(); i++) {
-            sum += Integer.parseInt(String.valueOf(number.charAt(i)));
+            sum += number.charAt(i) - '0';
         }
         
         answer = sum % 9;
