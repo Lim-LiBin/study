023687031292151d76ff50e773974146cc51cf82
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2024-12-29 20:52:59 +0900

    [level 0] Title: 배열 만들기 2, Time: 598.43 ms, Memory: 403 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for(int i = l; i <= r; i++) {
+            String num = String.valueOf(i);
+            
+            num = num.replaceAll("5","").replaceAll("0","");
+            if(num.isEmpty()) arr.add(i);
+        }
+        
+        if (arr.isEmpty()) {
+            return new int[] {-1};
+        }
+        
+        int[] answer = new int[arr.size()];
+        
+        for(int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
