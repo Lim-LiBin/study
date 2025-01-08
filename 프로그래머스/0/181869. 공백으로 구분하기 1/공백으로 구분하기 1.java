be16e30c60860ac7806bb5d29be13d030b859841
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-01-08 14:38:59 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.58 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        StringTokenizer st = new StringTokenizer(my_string);
+        
+        int count = st.countTokens();
+        String[] answer = new String[count];
+        
+        for(int i = 0; i < count; i++) {
+            answer[i] = st.nextToken();
+        }
+        
+        return answer;
+    }
+}
