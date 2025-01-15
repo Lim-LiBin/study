51b236dca73bbbfa2f6e62c376b178e1fdaaa60c
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-01-15 16:57:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.36 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         String evenS = "수";
         String oddS = "박";
         
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) answer += evenS;
-            else answer += oddS;
+            if (i % 2 == 0) sb.append(evenS);
+            else sb.append(oddS);
         }
         
-        return answer;
+        return sb.toString();
     }
 }
