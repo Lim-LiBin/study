e07f268875b552bc7e40b6f9fd1bb387f116cf06
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-01-17 18:54:21 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        
+        for (int n : d) {
+            sum += n;
+            if (sum > budget) break;
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
