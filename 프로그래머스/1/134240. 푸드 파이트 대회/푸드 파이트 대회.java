7452cec3b2ce6468f3e9432573aa53f7be94e53d
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-01-19 20:55:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.57 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] / 2 >= 1) {
+                for (int j = 0; j < food[i] / 2; j++) {
+                    sb.append(String.valueOf(i));
+                }
+            }
+        }
+        
+        sb.append("0");
+        
+        for (int i = food.length - 1; i >= 1; i--) {
+            if (food[i] / 2 >= 1) {
+                for (int j = 0; j < food[i] / 2; j++) {
+                    sb.append(String.valueOf(i));
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
+}
