eeaf9d69fcf0347570facced74d9667abf8bba5b
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-01-22 22:56:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        
+        int p1 = 0, p2 = 0;
+        for (int i = 0; i < goal.length; i++) {
+            if (p1 < cards1.length && goal[i].equals(cards1[p1])) {
+                p1++;
+            } else if (p2 < cards2.length && goal[i].equals(cards2[p2])) {
+                p2++;
+            } else return "No";
+        }
+        
+        return answer;
+    }
+}
