3746322cff40dc620ae0dfcc971da038959a11dd
Lim Li Bin <137471837+Lim-LiBin@users.noreply.github.com>
2025-02-06 22:16:43 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws IOException {
+	    System.out.println("112");
+	    System.out.println("lab1018");
+	}
+}
